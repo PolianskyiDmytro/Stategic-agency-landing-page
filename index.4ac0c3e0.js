eddbfc7277bde2ac5fbc33f6d38b1f8c5c710fcd
Polianskyi Dmytro <dimdemon1@gmail.com>
2026-04-05 12:36:07 +0300

    make build

---
+++ index.4ac0c3e0.js
@@ -0,0 +1,2 @@
+var o=document.querySelector(".dropdown");o.querySelector(".dropdown__icon").addEventListener("click",function(){o.classList.contains("dropdown--active")?o.classList.remove("dropdown--active"):o.classList.add("dropdown--active")});
+//# sourceMappingURL=index.4ac0c3e0.js.map
